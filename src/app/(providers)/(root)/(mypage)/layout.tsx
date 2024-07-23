8429b8b00fbe968cd1626dbf8482d1e85eb22905
suzy <tnwl9540@gmail.com>
2024-07-23 16:21:50 +0900

    💄 style : 글로벌, mypage레이아웃 수정

--- src/app/(providers)/(root)/(mypage)/layout.tsx
+++ src/app/(providers)/(root)/(mypage)/layout.tsx
@@ -2,7 +2,7 @@ import MyPageSideBar from '@/components/mypage/MyPageSideBar';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-row'>
+    <div className='flex flex-row px-36 mt-32'>
       <MyPageSideBar />
       <main>{children}</main>
     </div>
