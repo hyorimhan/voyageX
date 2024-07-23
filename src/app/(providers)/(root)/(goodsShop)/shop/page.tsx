a86a5f3feccc05724f9c059a601549a0b99bdb38
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-23 19:19:20 +0900

    add : 굿즈샵 페이지 생성

--- src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
+++ src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import Page from '@/components/pages/Page';
+import GoodsList from '@/components/shop/GoodsList';
 
 const ShopPage = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Page>
+        <h1 className='border-b-2 border-white text-4xl font-bold text-white mt-40 py-4'>
+          GOODS SHOP
+        </h1>
+        <GoodsList />
+      </Page>
+    </>
+  );
 };
 
 export default ShopPage;
