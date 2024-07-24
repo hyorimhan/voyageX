fed7702d0862f9ba584f66892f08e319a9ed3aa0
SNGCHN <sngchn0312@naver.com>
2024-07-24 20:12:36 +0900

    tailwind config 색상 커스텀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,17 +9,46 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'purple': {
-          300: '#9b5de5',
-          500: '#f15bb5',
-          700: '#fee440',
-          900: '#00bbf9', 
+        primary: {
+          50: '#F0EDF7',
+          100: '#D3CAE8',
+          200: '#B6A6D8',
+          300: '#9983C9',
+          400: '#7C5FB9',
+          500: '#6346A0',
+          600: '#4D367C',
+          700: '#372759',
+        },
+        black: {
+          50: '#F4F4F4',
+          100: '#E0E0E0',
+          200: '#CCCCCC',
+          300: '#B8B8B8',
+          400: '#A3A3A3',
+          500: '#8F8F8F',
+          600: '#7A7A7A',
+          700: '#666666',
+          800: '#525252',
+          900: '#3D3D3D',
+        },
+        error: {
+          900: '#EE4700',
+        },
+        warning: {
+          900: '#',
+        },
+        info: {
+          900: '#',
+        },
+        success: {
+          900: '#51CC56',
+        },
+        dimmer: {
+          900: '#171719', //50%
         },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
