5220990fe30b76fa2620b5714acbe114027b7a06
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-24 15:06:15 +0900

    add : 굿즈샵 공통 레이아웃 추가

--- src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
+++ src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
@@ -5,9 +5,6 @@ const ShopPage = () => {
   return (
     <>
       <Page>
-        <h1 className='border-b-2 border-white text-4xl font-bold text-white mt-40 py-4'>
-          GOODS SHOP
-        </h1>
         <GoodsList />
       </Page>
     </>
