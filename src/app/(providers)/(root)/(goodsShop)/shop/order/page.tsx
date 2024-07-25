c3c92996a0568c8e9764fcc4439500a68d37ffec
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-25 10:45:01 +0900

    fix: 레이아웃 코드 수정

--- src/app/(providers)/(root)/(goodsShop)/shop/order/page.tsx
+++ src/app/(providers)/(root)/(goodsShop)/shop/order/page.tsx
@@ -1,9 +1,15 @@
+import Page from '@/components/pages/Page';
 import OrderForm from '@/components/shop/order/OrderForm';
 
 const OrderPage = () => {
   return (
     <>
-      <OrderForm />
+      <Page>
+        <h1 className='border-b-2 border-white text-lg font-bold text-white mt-40 py-4'>
+          GOODS SHOP
+        </h1>
+        <OrderForm />
+      </Page>
     </>
   );
 };
--- src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
+++ src/app/(providers)/(root)/(goodsShop)/shop/page.tsx
@@ -5,6 +5,9 @@ const ShopPage = () => {
   return (
     <>
       <Page>
+        <h1 className='border-b-2 border-white text-4xl font-bold text-white mt-40 py-4'>
+          GOODS SHOP
+        </h1>
         <GoodsList />
       </Page>
     </>
