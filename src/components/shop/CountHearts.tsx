064035b9accad0d57f5ca02a31305cb0d01ed9e5
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-26 16:39:12 +0900

    add: 굿즈의 좋아요 수 컴포넌트 추가

---
+++ src/components/shop/CountHearts.tsx
@@ -0,0 +1,13 @@
+import { IoHeart } from 'react-icons/io5';
+
+function CountHearts() {
+  return (
+    <>
+      <div className='flex flex-row text-xl items-center'>
+        <IoHeart /> 45
+      </div>
+    </>
+  );
+}
+
+export default CountHearts;
