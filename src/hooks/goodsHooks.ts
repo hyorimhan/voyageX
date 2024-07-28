2f4fbfdff01c73b5beb9c1db603d9b913b67f6cb
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-28 15:03:52 +0900

    add: 찜기능 관련된 service, hook 추가

--- src/hooks/goodsHooks.ts
+++ src/hooks/goodsHooks.ts
@@ -1,10 +1,31 @@
-import { getGoods } from '@/services/goods';
+import {
+  getGoods,
+  getIsLikeOfGoodsByUser,
+  toggleLikeGoods,
+} from '@/services/goods';
 import { Tables } from '@/types/supabase';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useGetOrderedGoods = (order: string) => {
   return useQuery<Tables<'goods'>[]>({
     queryKey: ['goods', { order }],
     queryFn: () => getGoods(order),
+  });
+};
+
+export const useGetLikedGoodsByUser = (goods_id: string, user_id: string) => {
+  return useQuery<Tables<'liked_goods'>[]>({
+    queryKey: ['like', goods_id, user_id],
+    queryFn: () => getIsLikeOfGoodsByUser(goods_id, user_id),
+  });
+};
+
+export const useToggleLikeGoods = (goods_id: string, user_id: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: toggleLikeGoods,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['like', goods_id, user_id] });
+    },
   });
 };
