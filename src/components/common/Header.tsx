1324730fbb76bad07627b0f9d8dcf0005dceda2c
hyorimhan <mynameishan8@gmail.com>
2024-07-28 16:58:05 +0900

    feat: 구글 로그인, 헤더 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -25,18 +25,18 @@ const Header = () => {
   return (
     <header className='bg-header-default bg-opacity-60 h-16 flex fixed z-20 top-0 items-center justify-between px-4 w-full mx-auto '>
       <div className='max-w-[1120px] mx-auto flex justify-between items-center w-full '>
-        <nav className='hidden md:flex items-center space-x-5 '>
+        <nav className='hidden md:flex items-center space-x-5 w-[260px]'>
           <Link href='/tour'>
-            <span className=' hover:text-gray-300 '>여행 상품</span>
+            <span className=' hover:text-gray-300'>여행 상품</span>
           </Link>
           <Link href='/shop'>
-            <span className=' hover:text-gray-300'>굿즈샵</span>
+            <span className=' hover:text-gray-300 '>굿즈샵</span>
           </Link>
           <Link href='/community'>
-            <span className=' hover:text-gray-300 '>커뮤니티</span>
+            <span className=' hover:text-gray-300 '>자유게시판</span>
           </Link>
           <Link href='/address_list'>
-            <span className=' hover:text-gray-300 '>마이 페이지</span>
+            <span className=' hover:text-gray-300 '>뉴스</span>
           </Link>
         </nav>
         <div className='flex items-center justify-center '>
@@ -44,8 +44,10 @@ const Header = () => {
             <span className='font-bold text-2xl '>Voyage X</span>
           </Link>
         </div>
-        <div className='flex items-center space-x-2 '>
-          <FaUserCircle className=' w-6 h-6 mr-[12px]' />
+        <div className='flex items-center justify-end space-x-2 w-[260px]'>
+          <Link href='/address_list'>
+            <FaUserCircle className=' w-6 h-6 ' />
+          </Link>
           {user ? (
             <LogoutBtn />
           ) : (
