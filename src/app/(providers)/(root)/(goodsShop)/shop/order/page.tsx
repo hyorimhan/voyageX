571132d34539d914fe2ce76b07a4120af0c0fd6d
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-30 17:07:30 +0900

    fix: OrderForm 컴포넌트 같이 쓸 수 있게 변경

--- src/app/(providers)/(root)/(goodsShop)/shop/order/page.tsx
+++ src/app/(providers)/(root)/(goodsShop)/shop/order/page.tsx
@@ -8,7 +8,7 @@ const OrderPage = () => {
         <h1 className='border-b-2 border-white text-lg font-bold text-white mt-20 py-4'>
           GOODS SHOP
         </h1>
-        <OrderForm />
+        <OrderForm isTour={true} />
       </Page>
     </>
   );
--- src/components/order/OrderForm.tsx
+++ src/components/order/OrderForm.tsx
@@ -1,16 +1,24 @@
+'use client';
+
 import ExpressInfo from './ExpressInfo';
 import PayButton from './PayButton';
 import ItemsInfo from './ItemsInfo';
 import CustomerInfo from './CustomerInfo';
 
-function OrderForm() {
+interface OrderFormPropsType {
+  isTour: boolean;
+}
+
+function OrderForm({ isTour }: OrderFormPropsType) {
   return (
     <>
       <div className='grid grid-cols-[minmax(0,1fr)_minmax(0,0.5fr)] gap-x-10'>
         <div className='flex flex-col items-start'>
-          <div className='mt-4 w-full'>
-            <ExpressInfo />
-          </div>
+          {!isTour && (
+            <div className='mt-4 w-full'>
+              <ExpressInfo />
+            </div>
+          )}
           <div className='mt-4 w-full'>
             <CustomerInfo />
           </div>
