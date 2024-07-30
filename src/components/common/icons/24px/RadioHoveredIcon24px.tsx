388e75e7927162aeb6fdbdfdfda62fd9fbadcf92
suzy <tnwl9540@gmail.com>
2024-07-31 00:37:47 +0900

    add : radio, check box 24px 아이콘 추가

---
+++ src/components/common/icons/24px/RadioHoveredIcon24px.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+const RadioHoveredIcon24px = () => {
+  return (
+    <>
+      <Image
+        src='/icons/24px/radio_hovered.svg'
+        alt='radio hovered icon 24px'
+        width={24}
+        height={24}
+      />
+    </>
+  );
+};
+
+export default RadioHoveredIcon24px;
