7a616eacf3152e7fbb6691bcac4c0f3e57b590ae
jcelee <kangmin@pthreemedia.com>
2024-08-09 11:09:35 +0900

    :sparkles: remove trash consolelog

--- src/components/main/VideoSection.tsx
+++ src/components/main/VideoSection.tsx
@@ -15,9 +15,7 @@ const VideoSection: React.FC<VideoSectionProps> = ({ videoSrc, heading, subHeadi
     setTimeout(() => {
       if (videoRef.current) {
         const videoElement = videoRef.current;
-        console.log(videoElement.readyState)
         const checkVideoLoaded = () => {
-          console.log("videoElementvideoElementvideoElementvideoElementvideoElement", videoElement)
           if (videoElement.readyState >= 3) {
             setVideoLoaded(true);
           }
--- src/hooks/useScrollTrigger.ts
+++ src/hooks/useScrollTrigger.ts
@@ -8,12 +8,9 @@ const useScrollTrigger = (
   videoLoaded: boolean,
   sectionsRef: React.MutableRefObject<(HTMLDivElement | null)[]>
 ) => {
-  console.log("sectionsRefsectionsRefsectionsRefsectionsRefsectionsRefsectionsRefsectionsRef", sectionsRef.current)
-  console.log("videoLoadedvideoLoadedvideoLoadedvideoLoadedvideoLoadedvideoLoadedvideoLoadedvideoLoaded", videoLoaded)
 
   useEffect(() => {
     if (videoLoaded && sectionsRef.current.length > 0) {
-      console.log(123123123123123123)
       sectionsRef.current.forEach((section) => {
         if (section) {
           ScrollTrigger.create({
