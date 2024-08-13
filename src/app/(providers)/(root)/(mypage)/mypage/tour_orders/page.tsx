e94003f7348518277bd97f2c92e6d920211349a1
suzy <tnwl9540@gmail.com>
2024-08-13 10:21:58 +0900

    style : 티켓 모바일변경 / 아이콘추가

--- src/app/(providers)/(root)/(mypage)/mypage/tour_orders/page.tsx
+++ src/app/(providers)/(root)/(mypage)/mypage/tour_orders/page.tsx
@@ -1,10 +1,15 @@
+import TourOrderListMobile from '@/components/mypage/tour_orders/TourOrderListMobile';
 import TourOrdersList from '@/components/mypage/tour_orders/TourOrdersList';
+import Link from 'next/link';
 
 const TourOrdersPage = () => {
   return (
     <div>
-      <p className='text-2xl mb-14'>여행상품 주문/배송조회</p>
+      <Link href={'/mypage/side_bar'} className='text-2xl mb-14'>
+        여행상품 주문/배송조회
+      </Link>
       <TourOrdersList />
+      <TourOrderListMobile />
     </div>
   );
 };
---
+++ src/components/common/icons/24px/ArrowRightIcon24px.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+const ArrowRightIcon24px = () => {
+  return (
+    <Image
+      src='/icons/24px/arrow_right.svg'
+      alt='arrow right 24px'
+      width={24}
+      height={24}
+    />
+  );
+};
+
+export default ArrowRightIcon24px;
