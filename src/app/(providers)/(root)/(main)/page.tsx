c9b644feb6e45313ae820c76ccf0f7c158444b9a
hwijinkim22 <bout9396@naver.com>
2024-08-16 01:44:17 +0900

    style: 4번째 섹션 반응형 구현

--- src/app/(providers)/(root)/(main)/page.tsx
+++ src/app/(providers)/(root)/(main)/page.tsx
@@ -195,7 +195,7 @@ const MainPage = () => {
   className='section section-bg h-screen flex flex-col items-center justify-center'
 >
   <h1
-    className={`text-4xl absolute font-semibold top-40 left-60 ${orbitron.className}
+    className={`text-4xl absolute font-semibold top-40 left-20 ${orbitron.className}
     sm:text-2xl sm:font-medium sm:top-16 sm:left-4`}
   >
     GOODS SHOP
@@ -209,68 +209,69 @@ const MainPage = () => {
       <Loading />
     </p>
   ) : (
-    <div className='grid grid-cols-3 gap-4 sm:grid-cols-2 sm:gap-2'>
-      {goods?.slice(0, 4).map((item, index) => (
-        <div
-          key={item.id}
-          className={`p-4 rounded shadow ${
-            index < 1 && 'hidden sm:block'
-          } sm:w-72 sm:h-72`}
-        >
-          <Link href={`shop_detail/${item.id}`}>
-            <Image
-              src={item.goods_img}
-              alt={item.goods_name}
-              width={320}
-              height={360}
-              className='object-cover sm:w-40 sm:h-32'
-            />
-            <div className='mt-4 sm:mt-2'>
-              <p className='bg-black-600 text-black-50 text-xs px-2 py-1 rounded-full mb-2 inline-block
-              sm:top-60s'>
-                무료 배송
-              </p>
-              <h2 className='text-base sm:text-xs font-medium text-white break-words'>
-                {item.goods_name}
-              </h2>
-              <p className='text-black-200 line-through sm:text-xs'>
-                {item.pre_price.toLocaleString()}원
-              </p>
-              <p className='text-sm sm:text-base sm:font-semibold'>
-                <span className='text-red-500 text-xl'>
-                  {item.discount}%
-                </span>{' '}
-                <span className='text-white text-xl'>
-                  {item.goods_price.toLocaleString()}원
-                </span>
-              </p>
-              <div className='flex items-center justify-between mt-2'>
-                <span className='flex items-center'>
-                  <Image
-                    src='/icons/20px/star_true.svg'
-                    alt='star icon'
-                    width={16}
-                    height={16}
-                    className='mr-1'
-                  />
-                  {Number(item.rating_avg).toFixed(1)}
-                </span>
-                <span className='flex items-center sm:hidden'>
-                  <Image
-                    src='/icons/20px/heart_default.svg'
-                    alt='heart icon'
-                    width={16}
-                    height={16}
-                    className='mr-1 sm:hidden'
-                  />
-                  {item.like_count}
-                </span>
-              </div>
-            </div>
-          </Link>
+    <ul className='grid grid-cols-3 gap-4 p-4 sm:grid-cols-2 sm:gap-2'>
+  {goods?.slice(0, 4).map((item, index) => (
+    <li
+      key={item.id}
+      className={`p-4 rounded shadow ${
+        index < 1 && 'hidden sm:block'
+      } sm:w-full sm:h-auto list-none`}
+    >
+      <Link href={`shop_detail/${item.id}`} className="flex flex-col">
+        <div className="">
+          <Image
+            src={item.goods_img}
+            alt={item.goods_name}
+            width={320}
+            height={360}
+            className='object-cover w-full h-72 sm:h-32'
+          />
+          <div className='mt-4 sm:mt-2'>
+            <p className='bg-black-600 text-black-50 text-xs px-2 py-1 rounded-full mb-2 inline-block'>
+              무료 배송
+            </p>
+            <h2 className='text-base sm:text-xs font-medium text-white break-words'>
+              {item.goods_name}
+            </h2>
+            <p className='text-black-200 line-through sm:text-xs'>
+              {item.pre_price.toLocaleString()}원
+            </p>
+            <p className='text-sm sm:text-base sm:font-semibold'>
+              <span className='text-red-500 text-xl'>
+                {item.discount}%
+              </span>{' '}
+              <span className='text-white text-xl'>
+                {item.goods_price.toLocaleString()}원
+              </span>
+            </p>
+          </div>
         </div>
-      ))}
-    </div>
+        <div className='flex items-center justify-between mt-2'>
+          <span className='flex items-center'>
+            <Image
+              src='/icons/20px/star_true.svg'
+              alt='star icon'
+              width={16}
+              height={16}
+              className='mr-1'
+            />
+            {Number(item.rating_avg).toFixed(1)}
+          </span>
+          <span className='flex items-center'>
+            <Image
+              src='/icons/20px/heart_default.svg'
+              alt='heart icon'
+              width={16}
+              height={16}
+              className='mr-1'
+            />
+            {item.like_count}
+          </span>
+        </div>
+      </Link>
+    </li>
+  ))}
+</ul>
   )}
 </section>
 
