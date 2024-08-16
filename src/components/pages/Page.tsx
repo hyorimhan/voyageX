ac38261274964ff80524e33b8afce33a9bf4b2b8
hyorimhan <mynameishan8@gmail.com>
2024-08-16 12:48:01 +0900

    style: css 수정

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -4,7 +4,7 @@ import BackgroundVideo from '../common/BackgroundVideo';
 const Page = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <div className='relative min-h-screen'>
+      <div className='relative'>
         <BackgroundVideo />
         <div className='relative mx-auto max-w-[1120px] sm:mt-[70px] lg:mt-[131px] text-black-50'>
           {children}
