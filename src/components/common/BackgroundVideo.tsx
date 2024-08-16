710874f70e734f1bf3cc299b4b8d22867b1ca2ee
hyorimhan <mynameishan8@gmail.com>
2024-08-16 11:36:11 +0900

    bugfix: 배경 안 보임

--- src/components/common/BackgroundVideo.tsx
+++ src/components/common/BackgroundVideo.tsx
@@ -1,6 +1,6 @@
 export default function BackgroundVideo() {
   return (
-    <div className='fixed top-0 left-0 w-full h-full -z-10 opacity-50'>
+    <div className='fixed top-0 left-0 w-full h-full -z-50'>
       <video
         src='/videos/backgroundVideo.mp4'
         autoPlay
@@ -9,6 +9,7 @@ export default function BackgroundVideo() {
         muted
         suppressHydrationWarning
       ></video>
+      <div className='absolute top-0 left-0 w-full h-full bg-black-1000 opacity-30'></div>
     </div>
   );
 }
