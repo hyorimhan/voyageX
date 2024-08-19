720a3dd017f2cdcd50326279ca7ab86f145da226
Ghastly Mouse <gusdnr0839@gmail.com>
2024-08-19 13:14:38 +0900

    add: 리뷰 패치라우트핸들러 api, 서비스 파일 추가

---
+++ src/app/api/review/tour/tour/[tour_id]/route.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@/supabase/server';
+import { NextResponse } from 'next/server';
+
+type ParamsType = {
+  params: { tour_id: string };
+};
+
+export const GET = async (request: Request, { params }: ParamsType) => {
+  const supabase = createClient();
+  const { tour_id } = params;
+  const { data, error } = await supabase
+    .from('tour_reviews')
+    .select('*, user:users(email)')
+    .eq('tour_id', tour_id);
+  if (error) return NextResponse.json({ error });
+  return NextResponse.json(data);
+};
