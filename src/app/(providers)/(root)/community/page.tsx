aabfdfacd6ba24247b0fb15560f6827004175282
SNGCHN <sngchn0312@naver.com>
2024-08-20 11:09:56 +0900

    커뮤니티 반응형 수정

--- src/app/(providers)/(root)/community/page.tsx
+++ src/app/(providers)/(root)/community/page.tsx
@@ -7,15 +7,17 @@ import CommunitySearch from '@/components/community/main/CommunitySearch';
 
 const CommunityPage = () => {
   return (
-    <div className='flex flex-col justify-between mb-14 mt-26 sm:w-full sm:gap-7 sm:p-5 sm:mb-4 md:p-11 md:mb-6 md:mt-16 lg:mb-14'>
-      <h1
-        className={`text-[36px] font-bold text-white text-left ${orbitron.className} sm:text-left`}
-      >
-        <Link href='/community'>FREE BOARD</Link>
-      </h1>
-      <div className='flex justify-end items-center sm:w-full'>
-        <div className='w-full'>
-          <CommunitySearch />
+    <div className='flex flex-col mt-26 sm:w-full sm:gap-7 sm:p-5 sm:mb-4 md:p-11 md:mb-6 md:mt-16 lg:mb-14'>
+      <div className='flex justify-between sm:flex-col sm:w-full sm:gap-4 md:mb-14 lg:mb-14'>
+        <h1
+          className={`text-[36px] font-bold text-white ${orbitron.className}`}
+        >
+          <Link href='/community'>FREE BOARD</Link>
+        </h1>
+        <div className='flex justify-end items-center sm:w-full sm:mt-4'>
+          <div className='w-full'>
+            <CommunitySearch />
+          </div>
         </div>
       </div>
       <div className='flex justify-between items-center mb-11 sm:flex-col sm:gap-8'>
