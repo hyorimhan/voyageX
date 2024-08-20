9abe6304a36de55518f4497f96a4283d49b1f5ac
SNGCHN <sngchn0312@naver.com>
2024-08-20 10:58:33 +0900

    뉴스 페이지 검색 기능 추가

--- src/app/(providers)/(root)/news/search/page.tsx
+++ src/app/(providers)/(root)/news/search/page.tsx
@@ -1,26 +1,29 @@
-import NewsList from '@/components/news/NewsList';
-import { orbitron } from '../../../../../public/fonts/orbitron';
-import Link from 'next/link';
 import NewsSearch from '@/components/news/NewsSearch';
+import NewsSearchResult from '@/components/news/NewsSearchResult';
+import Link from 'next/link';
+import { orbitron } from '../../../../../../public/fonts/orbitron';
 
-const NewsPage = () => {
+function NewsSearchPage({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
   return (
-    <div>
+    <>
       <div className='flex justify-between mb-14 mt-26 sm:flex-col sm:w-full sm:gap-7 sm:p-5 sm:mb-4 md:p-11 md:mb-6 md:mt-16 lg:mb-14'>
         <h1
           className={`text-[36px] font-bold text-white text-left ${orbitron.className} sm:text-left`}
         >
           <Link href='/news'>NEWS</Link>
         </h1>
-        <div className='flex justify-end items-center sm:w-full'>
+        <div className='flex justify-end items-center mb-11 sm:w-full'>
           <div className='w-full'>
             <NewsSearch />
           </div>
         </div>
       </div>
-      <NewsList />
-    </div>
+      <NewsSearchResult searchValue={searchParams.search} />
+    </>
   );
-};
-
-export default NewsPage;
+}
+export default NewsSearchPage;
