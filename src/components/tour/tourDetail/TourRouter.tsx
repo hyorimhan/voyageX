fbede7965d1d8f5d97bd294394a9b6172c210b39
suzy <tnwl9540@gmail.com>
2024-08-21 10:01:48 +0900

    Merge branch 'dev' of https://github.com/hyorimhan/voyageX into feat-userpage

--- src/components/tour/tourDetail/TourRouter.tsx
+++ src/components/tour/tourDetail/TourRouter.tsx
@@ -14,10 +14,7 @@ function TourRouter({ url }: { url: string }) {
         router.replace(`${url}`);
       }}
     >
-      <div className='flex items-center mb-4 gap-4 mt-[100px] sm:mx-5'>
-        <div className='md:hidden lg:hidden'>
-          <ArrowLeftIcon24px />
-        </div>
+      <div className='flex items-center mb-4 gap-4 mt-[100px]'>
         <div
           className={` md:text-4xl lg:text-4xl font-semibold text-white sm:text-xl ${orbitron.className}`}
         >
